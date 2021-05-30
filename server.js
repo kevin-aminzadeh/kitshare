ff3edbe7e89ff2eef487c1a931633aa7a287fd04
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-30 15:31:06 +0930

    refactor: Move frontend route to router logic

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const path = require("path");
+const routes = require("./routes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,12 +14,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 app.use(routes);
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
