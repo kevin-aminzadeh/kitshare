66fc9a346da2e5821d093a6738c9a2d155aff371
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-30 13:13:51 +0930

    chore: Configure ESLint and Prettier linting and code formatting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
