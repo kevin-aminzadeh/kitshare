cc425bfc4fba5d6409801daf3ed7bce75425ea4e
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 00:25:24 +0930

    feat: Implement Listings service

--- services/ListingService.js
+++ services/ListingService.js
@@ -2,30 +2,53 @@ const { Listing } = require('../models/index');
 
 // Create New Listing in DB
 exports.createListing = async (ListingData) => {
-  const dbData = await Listing.create(ListingData);
-  return dbData;
+  try {
+    const dbData = await Listing.create(ListingData);
+    return dbData;
+  } catch (err) {
+    return err;
+  }
+};
+
+exports.getAll = async () => {
+  try {
+    const dbData = await Listing.findAll();
+    return dbData;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Update Listing Record
 exports.updateListing = async (ListingData) => {
-  const res = await Listing.update(
-    { content: ListingData.content },
-    {
-      where: {
-        id: ListingData.id,
-      },
-    }
-  );
-  return res;
+  try {
+    const res = await Listing.update(
+      { content: ListingData.content },
+      {
+        where: {
+          id: ListingData.id,
+          owner_id: ListingData.owner_id,
+        },
+      }
+    );
+    return res;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Delete Listing Record
 exports.deleteListing = async (ListingId, userId) => {
-  const res = await Listing.destroy({
-    where: {
-      id: ListingId,
-      owner_id: userId,
-    },
-  });
-  return res;
+  try {
+    const res = await Listing.destroy({
+      // Ensure userID === listing owner's ID
+      where: {
+        id: ListingId,
+        owner_id: userId,
+      },
+    });
+    return res;
+  } catch (err) {
+    return err;
+  }
 };
