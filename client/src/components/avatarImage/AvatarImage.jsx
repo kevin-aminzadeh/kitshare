b20e4077d05a03439d8d645670979d7f9cfb4d49
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-31 16:32:44 +0930

    feat: Implement avatarImage component

---
+++ client/src/components/avatarImage/AvatarImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AvatarImage() {
+  return (
+    <img
+      src="https://picsum.photos/seed/pic/200/300"
+      alt=""
+      className="rounded-circle"
+      style={{ width: '48px', height: '48px' }}
+    />
+  );
+}
+
+export default AvatarImage;
