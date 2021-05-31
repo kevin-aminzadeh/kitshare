1011c5c0dad84a112618e8f01e7cb6ac10228c67
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-31 17:21:47 +0930

    chore: Update codestyles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './components/pages/landing/Landing';
+import Navbar from './components/navbar/Navbar';
+import Listings from './components/pages/listings/Listings';
+import ListingDetails from './components/pages/listingDetails/ListingDetails';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Switch>
-          <Route path="/" render={() => <h1>Landing Page</h1>} />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/listings" component={Listings} />
+          <Route exact path="/listings/cameras" component={Listings} />
+          <Route exact path="/listings/lenses" component={Listings} />
+          <Route exact path="/listings/lighting" component={Listings} />
+          <Route exact path="/listings/audio" component={Listings} />
+          <Route path="/listings/1" component={ListingDetails} />
         </Switch>
       </div>
     </Router>
