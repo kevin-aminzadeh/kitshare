7a720ed0177644058ae394578e94afcbff4dfd80
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 00:25:46 +0930

    feat: Implement Listings controller

--- controllers/ListingController.js
+++ controllers/ListingController.js
@@ -5,24 +5,44 @@ exports.createListing = async (req, res) => {
   try {
     // If User is Not Logged in, Reject Request
     if (!req.session.loggedIn) {
-      throw Error('You Must Be Logged In To Listing.');
+      throw Error('You must be logged in to create a new listing.');
     }
 
     // If Request Data is Invalid, Reject Request
-    if (!req.body.content || !req.body.postId) {
+    if (
+      !req.body.title ||
+      !req.body.description ||
+      !req.body.price ||
+      !req.body.priceIntervalId ||
+      !req.body.locationId ||
+      !req.body.ownerId
+    ) {
       throw Error('Invalid Request Data.');
     }
 
     // Create Listing Object
     const Listing = {
-      content: req.body.content,
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      price_interval_id: req.body.priceIntervalId,
+      location_id: req.body.locationId,
       owner_id: req.session.userId,
-      post_id: req.body.postId,
     };
 
     // Create Listing in DB
     await ListingService.createListing(Listing);
     res.status(200).json('Listing Created Successfully.');
+  } catch (err) {
+    res.status(400).json(err.toString());
+  }
+};
+
+// Get All Listings
+exports.getAllListings = async (req, res) => {
+  try {
+    const listings = ListingService.getAll();
+    res.status(200).json(listings);
   } catch (err) {
     res.status(400).json(err.toString());
   }
@@ -37,16 +57,19 @@ exports.updateListing = async (req, res) => {
     }
 
     // If Request Data is Invalid, Reject Request
-    if (!req.body.content || !req.body.postId || !req.body.id) {
+    if (!req.body) {
       throw Error('Invalid Request Data.');
     }
 
     // Construct Listing Object
     const Listing = {
       id: req.body.id,
-      content: req.body.content,
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      price_interval_id: req.body.priceIntervalId,
+      location_id: req.body.locationId,
       owner_id: req.session.userId,
-      post_id: req.body.postId,
     };
 
     // Update Listing
@@ -71,7 +94,7 @@ exports.deleteListing = async (req, res) => {
       throw Error('Invalid Request Data.');
     }
 
-    // Delete Listing
+    // Request Listing Deletion from Listing Service
     await ListingService.deleteListing(req.params.id, req.session.userId);
 
     res.status(200).json('Listing Successfully Deleted');
