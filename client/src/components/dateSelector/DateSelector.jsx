83912393d19ccaca240a081cfa264dbf020153b4
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-31 16:38:39 +0930

    feat: Implement DateSelector component boilerplate

---
+++ client/src/components/dateSelector/DateSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DateSelector() {
+  return (
+    <div className="container">
+      <div className="row" />
+    </div>
+  );
+}
+
+export default DateSelector;
