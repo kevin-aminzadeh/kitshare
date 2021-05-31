8751c3604c1a8650f859d4fa3482ed84b9d34bdb
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 00:24:51 +0930

    chore: Update ESLint prettier EOL config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
