62eee36911ef82c4c1913b1b2e16e15f77074de2
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 14:02:43 +0930

    feat: Define model relationships

--- models/index.js
+++ models/index.js
@@ -1,3 +1,99 @@
+const User = require('./User');
 const Listing = require('./Listing');
+const PaymentMethod = require('./PaymentMethod');
+const UserRole = require('./UserRole');
+const Location = require('./Location');
+const Booking = require('./Booking');
+const Status = require('./Status');
+const PriceInterval = require('./PriceInterval');
+
+// User and Listing Model Relationships
+User.hasMany(Listing, {
+  foreignKey: 'owner_id',
+  onDelete: 'CASCADE',
+});
+
+Listing.belongsTo(User, {
+  foreignKey: 'owner_id',
+  as: 'owner',
+});
+
+// User and PaymentMethod Model Relationships
+User.hasMany(PaymentMethod, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+PaymentMethod.belongsTo(User, {
+  foreignKey: 'user_id',
+  as: 'owner',
+});
+
+// User and UserRole Model Relationships
+User.belongsTo(UserRole, {
+  foreignKey: 'role_id',
+});
+
+UserRole.hasMany(User, {
+  foreignKey: 'role_id',
+});
+
+// User and Location Model Relationships
+User.belongsTo(Location, {
+  foreignKey: 'location_id',
+});
+
+Location.hasMany(User, {
+  foreignKey: 'location_id',
+  onDelete: 'SET NULL',
+});
+
+// User and Booking Model Relationships
+User.hasMany(Booking, {
+  foreignKey: 'customer_id',
+  onDelete: 'CASCADE',
+});
+
+Booking.belongsTo(User, {
+  foreignKey: 'customer_id',
+});
+
+// Booking and Listing Relationships
+Listing.hasMany(Booking, {
+  foreignKey: 'listing_id',
+  onDelete: 'CASCADE',
+});
+
+Booking.belongsTo(Listing, {
+  foreignKey: 'listing_id',
+});
+
+// Booking and Status Relationships
+Status.hasMany(Booking, {
+  foreignKey: 'status_id',
+});
+
+Booking.belongsTo(Status, {
+  foreignKey: 'status_id',
+});
+
+// Listing and PriceInterval Relationships
+PriceInterval.hasMany(Listing, {
+  foreignKey: 'price_interval_id',
+});
+
+Listing.belongsTo(PriceInterval, {
+  foreignKey: 'price_interval_id',
+});
+
+// Listing and Location Relationships
+Location.hasMany(Listing, {
+  foreignKey: 'location_id',
+  onDelete: 'SET NULL',
+});
+
+Listing.belongsTo(Location, {
+  foreignKey: 'location_id',
+});
 
 module.exports = { Listing };
