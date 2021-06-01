88d1e4b1e52137fe7c4a32956632a64d58922a7e
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 11:48:45 +0930

    feat: Implement User model schema

---
+++ models/User.js
@@ -0,0 +1,100 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(attempt) {
+    return bcrypt.compareSync(attempt, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    dob: {
+      type: DataTypes.DATE,
+      validate: {
+        isBefore: Date.now,
+      },
+    },
+    street_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mobile: {
+      type: DataTypes.NUMBER,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 50],
+      },
+    },
+    is_verified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    user_type_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'userType',
+        key: 'id',
+      },
+    },
+    location_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'location',
+        key: 'id',
+      },
+    },
+  },
+  {
+    hooks: {
+      // Hash User Password On Create
+      beforeCreate: async (userData) => {
+        const newUserData = { ...userData };
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // Hash User Password On Update
+      beforeUpdate: async (userData) => {
+        const updatedUserData = { ...userData };
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
