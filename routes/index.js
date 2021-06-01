78965081724d0e1168e51f2a0db70588b224d18f
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-02 02:51:54 +0930

    fix: Use wildcard route to serve frontend

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/api', apiRoutes);
 
 // If no API routes are hit, send the React App
 if (process.env.NODE_ENV === 'production') {
-  router.get('/*', (req, res) => {
+  router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'), (err) => {
       if (err) {
         res.status(500).send(err);
