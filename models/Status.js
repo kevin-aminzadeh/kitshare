0a4901cc2dbe0504929369fc03b8f5ef9b57732f
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-01 12:01:28 +0930

    feat: Define Status model schema

---
+++ models/Status.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Status extends Model {}
+
+Status.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Status',
+  }
+);
+
+module.exports = Status;
