61dbed1fb92a04dc36ea1adc4e16e46423d82d3e
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-05 19:41:44 +0930

    feat: Implement booking routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const listingRoutes = require('./listingRoutes');
+const bookingRoutes = require('./bookingRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/listings', listingRoutes);
+router.use('/bookings', bookingRoutes);
 
 module.exports = router;
