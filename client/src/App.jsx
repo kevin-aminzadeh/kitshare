ff22ed6ad998c670eb739caca5a953b584f5603a
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-06 18:05:30 +0930

    feat: Implement AuthProvider root level value update logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './components/pages/landing/Landing';
 import Navbar from './components/navbar/Navbar';
 import Listings from './components/pages/listings/Listings';
 import ListingDetails from './components/pages/listingDetails/ListingDetails';
+import { AuthProvider } from './utils/authContext';
+import Login from './components/pages/login/Login';
+import API from './utils/API';
 
 function App() {
+  const [userState, setUserState] = useState({});
+
+  const fetchUserData = async () => {
+    const userData = await API.checkSession();
+
+    setUserState({ ...userData.user });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/listings" component={Listings} />
-          <Route exact path="/listings/cameras" component={Listings} />
-          <Route exact path="/listings/lenses" component={Listings} />
-          <Route exact path="/listings/lighting" component={Listings} />
-          <Route exact path="/listings/audio" component={Listings} />
-          <Route exact path="/listings/:id" component={ListingDetails} />
-        </Switch>
-      </div>
-    </Router>
+    <AuthProvider user={userState}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/listings" component={Listings} />
+            <Route exact path="/listings/cameras" component={Listings} />
+            <Route exact path="/listings/lenses" component={Listings} />
+            <Route exact path="/listings/lighting" component={Listings} />
+            <Route exact path="/listings/audio" component={Listings} />
+            <Route exact path="/listings/:id" component={ListingDetails} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
