f6a17710168159ba5207cdf6a64ad73717310bf1
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-07 01:35:43 +0930

    chore: Add past booking seed for testing

--- seeds/seedBooking.js
+++ seeds/seedBooking.js
@@ -22,6 +22,13 @@ const bookingData = [
     listing_id: '2a708c58-c15f-4f37-923b-a9158d968bfb',
     customer_id: '11293394-4170-4764-8260-f4722c88ca3c',
   },
+  {
+    time_from: new Date('2021-06-01'),
+    time_to: new Date('2021-06-03'),
+    price_total: 166.0,
+    listing_id: '2a708c58-c15f-4f37-923b-a9158d968bfb',
+    customer_id: '11293394-4170-4764-8260-f4722c88ca3c',
+  },
 ];
 
 const seedBooking = async () => {
