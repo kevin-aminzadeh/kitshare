f2f1f80f69e50e7b6a87f4f5c7d6b08b7f68eec1
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-07 01:37:27 +0930

    refactor: Update ImageCard styling logic for better component reusability

--- client/src/components/imageCard/ImageCard.jsx
+++ client/src/components/imageCard/ImageCard.jsx
@@ -4,15 +4,21 @@ import PropTypes from 'prop-types';
 function ImageCard({ isSquare, rounded, imageSrc }) {
   return (
     <div
-      className="card-wrapper d-flex h-100 w-100 position-relative"
-      style={isSquare ? { paddingBottom: '100%' } : {}}
+      className="card-wrapper d-flex w-100 position-relative"
+      style={isSquare ? { paddingBottom: '100%' } : { height: '180px' }}
     >
-      <picture className={isSquare ? 'd-flex position-absolute top-0 bottom-0' : ''}>
+      <picture className={isSquare ? 'd-flex position-absolute top-0 bottom-0 w-100' : 'w-100'}>
         <img
           src={imageSrc}
           alt=""
           className={`rounded-${rounded ? '3' : '0'}`}
-          style={{ objectFit: 'cover', verticalAlign: 'bottom', width: '100%', height: '100%' }}
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+            verticalAlign: 'bottom',
+            width: '100%',
+            height: '100%',
+          }}
         />
       </picture>
     </div>
