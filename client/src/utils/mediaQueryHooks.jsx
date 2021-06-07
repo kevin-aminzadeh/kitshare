7b0ecac3099c865f08280dfcac0d2591c96f7aee
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-07 16:56:53 +0930

    refactor: Implement react-responsive media query hooks

---
+++ client/src/utils/mediaQueryHooks.jsx
@@ -0,0 +1,7 @@
+import { useMediaQuery } from 'react-responsive';
+
+export const useSmMediaQuery = () => useMediaQuery({ query: '(min-width: 576px)' });
+export const useMdMediaQuery = () => useMediaQuery({ query: '(min-width: 768px)' });
+export const useLgMediaQuery = () => useMediaQuery({ query: '(min-width: 992px)' });
+export const useXlMediaQuery = () => useMediaQuery({ query: '(min-width: 1200px)' });
+export const useXxlMediaQuery = () => useMediaQuery({ query: '(min-width: 1400px)' });
