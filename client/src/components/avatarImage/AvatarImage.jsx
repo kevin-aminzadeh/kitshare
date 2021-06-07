dbb8dd4fc9e43806b1f3b9c817e4231c2c6546eb
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-07 15:25:27 +0930

    refactor: Improve AvatarImage responsive behaviour

--- client/src/components/avatarImage/AvatarImage.jsx
+++ client/src/components/avatarImage/AvatarImage.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 function AvatarImage() {
   return (
-    <img
-      src="https://picsum.photos/seed/pic/200/300"
-      alt=""
-      className="rounded-circle"
-      style={{ width: '48px', height: '48px' }}
-    />
+    <div className="d-flex w-100 position-relative" style={{ paddingBottom: '100%' }}>
+      <picture className="d-flex position-absolute top-0 bottom-0 w-100">
+        <img
+          src="https://picsum.photos/seed/pic/200/300"
+          alt=""
+          className="rounded-circle"
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+            verticalAlign: 'bottom',
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </picture>
+    </div>
   );
 }
 
