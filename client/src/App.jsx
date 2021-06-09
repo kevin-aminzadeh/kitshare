a0c828191c6a4ae097303d4c4edc082dc1509db6
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-09 15:51:43 +0930

    feat: Implement Stripe context and protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Stripe Imports
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import LandingPage from './components/pages/landing/Landing';
 import Listings from './components/pages/listings/Listings';
 import ListingDetails from './components/pages/listingDetails/ListingDetails';
@@ -9,13 +12,18 @@ import Login from './components/pages/login/Login';
 import API from './utils/API';
 import Bookings from './components/pages/bookings/Bookings';
 import Profile from './components/pages/profile/Profile';
+import ProtectedRoute from './components/core/protectedRoute/ProtectedRoute';
+
+// Stripe Imports
+const stripePromise = loadStripe(
+  process.env.STRIPE_PUBLIC_KEY || 'pk_test_ggxGmL1SDbug6gG6sBMFuCMw00XCD4YY06'
+);
 
 function App() {
   const [userState, setUserState] = useState({});
 
   const fetchUserData = async () => {
     const userData = await API.checkSession();
-
     setUserState({ ...userData.user });
   };
 
@@ -25,22 +33,34 @@ function App() {
 
   return (
     <AuthProvider user={userState}>
-      <Router>
-        <div className="App h-100 d-flex flex-column">
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/bookings" component={Bookings} />
-            <Route exact path="/listings" component={Listings} />
-            <Route exact path="/listings/cameras" component={Listings} />
-            <Route exact path="/listings/lenses" component={Listings} />
-            <Route exact path="/listings/lighting" component={Listings} />
-            <Route exact path="/listings/audio" component={Listings} />
-            <Route exact path="/listings/:id" component={ListingDetails} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/account-settings" component={Profile} />
-          </Switch>
-        </div>
-      </Router>
+      <Elements stripe={stripePromise}>
+        <Router>
+          <div className="App h-100 d-flex flex-column">
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <ProtectedRoute
+                exact
+                path="/bookings"
+                component={Bookings}
+                isLoggedIn={!!userState.id}
+              />
+              <Route exact path="/listings" component={Listings} />
+              <Route exact path="/listings/cameras" component={Listings} />
+              <Route exact path="/listings/lenses" component={Listings} />
+              <Route exact path="/listings/lighting" component={Listings} />
+              <Route exact path="/listings/audio" component={Listings} />
+              <Route exact path="/listings/:id" component={ListingDetails} />
+              <ProtectedRoute exact path="/login" component={Login} isLoggedIn={!userState.id} />
+              <ProtectedRoute
+                exact
+                path="/account-settings"
+                component={Profile}
+                isLoggedIn={!!userState.id}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </Elements>
     </AuthProvider>
   );
 }
