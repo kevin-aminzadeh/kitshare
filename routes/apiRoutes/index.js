15f0ff33172a9e4ca415cb467dbefa6fe7dacc3d
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-09 11:56:56 +0930

    feat: Implement billing routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const listingRoutes = require('./listingRoutes');
 const bookingRoutes = require('./bookingRoutes');
+const billingRoutes = require('./billingRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/listings', listingRoutes);
 router.use('/bookings', bookingRoutes);
+router.use('/billing', billingRoutes);
 
 module.exports = router;
