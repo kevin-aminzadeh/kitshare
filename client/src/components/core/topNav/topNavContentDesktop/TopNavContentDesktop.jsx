3ee7629b833e6504bc93c226120eb04b2596eacd
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-09 17:59:28 +0930

    feat: Implement TopNav user profile dropdown

--- client/src/components/core/topNav/topNavContentDesktop/TopNavContentDesktop.jsx
+++ client/src/components/core/topNav/topNavContentDesktop/TopNavContentDesktop.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import API from '../../../../utils/API';
 import { useAuth } from '../../../../utils/authContext';
+import Dropdown from '../../../dropdown/Dropdown';
 
 function TopNavContentDesktop() {
-  const { currentUser } = useAuth();
+  const { currentUser, setCurrentUser } = useAuth();
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await API.logout();
+
+    setCurrentUser({});
+    history.goBack();
+  };
+
   return (
     <>
       <div className="col">
@@ -26,16 +38,28 @@ function TopNavContentDesktop() {
           {currentUser.id ? (
             <ul className="navbar-nav mb-2 mb-lg-0">
               <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle text-capitalize"
-                  href="/"
+                <button
+                  className="nav-link btn btn-link dropdown-toggle text-capitalize"
                   id="navbarDropdown"
-                  role="button"
+                  type="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
+                  onClick={() => setDropDownOpen(true)}
                 >
                   Hi, {currentUser.firstName}
-                </a>
+                </button>
+                <Dropdown isOpen={dropDownOpen}>
+                  <li>
+                    <Link className="dropdown-item" to="/account-settings">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <button type="button" className="dropdown-item" onClick={() => handleLogout()}>
+                      Logout
+                    </button>
+                  </li>
+                </Dropdown>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
                     <a className="dropdown-item" href="/">
