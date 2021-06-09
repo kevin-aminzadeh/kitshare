3346dc92dca8ddd31b938a5404b16c35d93967d3
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-09 15:49:00 +0930

    refactor: Add useXsMediaQuery hook

--- client/src/utils/mediaQueryHooks.jsx
+++ client/src/utils/mediaQueryHooks.jsx
@@ -1,5 +1,6 @@
 import { useMediaQuery } from 'react-responsive';
 
+export const useXsMediaQuery = () => useMediaQuery({ query: '(min-width: 360px)' });
 export const useSmMediaQuery = () => useMediaQuery({ query: '(min-width: 576px)' });
 export const useMdMediaQuery = () => useMediaQuery({ query: '(min-width: 768px)' });
 export const useLgMediaQuery = () => useMediaQuery({ query: '(min-width: 992px)' });
