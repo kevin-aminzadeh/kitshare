88ebcc518ee71b46e7d1fdce03de5f9dc2a8bc04
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-06-09 15:55:07 +0930

    chore: Create BillingController boilerplate

---
+++ controllers/BillingController.js
@@ -0,0 +1,46 @@
+const {
+  createPaymentIntent,
+  createStripeCustomer,
+  createSetupIntent,
+} = require('../services/StripeService');
+
+// Get User's Payment Methods
+exports.getPaymentMethods = async (req, res) => {
+  try {
+    // If User is Not Logged In Throw Error
+    if (!req.session.loggedIn) {
+      throw Error('Unauthorized.');
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Create New Payment Method
+exports.addNewPaymentMethod = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Update Existing Payment Method
+exports.updatePaymentMethod = async (req, res) => {};
+
+// Delete Existing Payment Method
+exports.deletePaymentMethod = async (req, res) => {};
+
+// Get User's Payout Methods
+exports.getPayoutMethods = async (req, res) => {};
+
+// Create New Payout Method
+exports.addNewPayoutMethod = async (req, res) => {};
+
+// Update Existing Payout Method
+exports.updatePayoutMethod = async (req, res) => {};
+
+// Delete Existing Payout Method
+exports.deletePayoutMethod = async (req, res) => {};
+
+// Process Payment
+exports.processPayment = async (req, res) => {};
